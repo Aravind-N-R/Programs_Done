53a6f72d46a537dcfa117e40a5f9beb1c2d0eb98
Aravind-N-R <155415467+Aravind-N-R@users.noreply.github.com>
2024-12-28 21:07:57 +0530

    Create config.py
    Config files for the reddit_bot.py file

---
+++ config.py
@@ -0,0 +1,8 @@
+#this file contains the code for the config import in the reddit_bot file
+#due to security reasons like username, passwords etc being available, this file will give a brief overview of the code
+#Code:
+username = "Username as per reddit"
+password ="Password for reddit account"
+client_id ="Client id that you get when creating a new reddit app"
+client_secret ="Client secret that you get when creating the reddit app"
+#All the values must be enclosed in double quotes
